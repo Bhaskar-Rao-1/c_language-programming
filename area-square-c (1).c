cf31f2c7456f2354139407950623a17399496f55
Bantupalli Bhaskara Rao <127121617+Bhaskar-Rao-1@users.noreply.github.com>
2024-07-11 17:56:59 +0530

    Add files via upload

---
+++ area-square-c (1).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int side;
+    
+    printf("enter square side\n");
+    scanf("%d",&side);
+    
+    int area=side*side;
+    
+    printf("area of square is %d",area);
+    
+    
+    return 0;
+}
